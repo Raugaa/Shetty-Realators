a5833dbde72a6e138b0bc68320a9e5b0fd4068d8
Raugaa <vyasgarv7@gmail.com>
2025-10-19 11:46:54 +0530

    number remove demo 4.0

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -82,7 +82,7 @@ const Contact = () => {
     {
       icon: Phone,
       title: "Call Us",
-      details: ["9422005110"],
+      details: ["9422005110" , "2"],
       color: "bg-dark-gray"
     },
     {
